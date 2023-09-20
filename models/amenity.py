34e478b27de48252ffaa5882288542128b50582a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-09-20 23:23:48 +0000

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State Module """
+from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Represent amenity"""
     __tablename__ = 'amenities'
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    place_amenities = relationship("Place", secondary=place_amenity)
