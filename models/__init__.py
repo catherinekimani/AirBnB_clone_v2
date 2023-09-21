2ea1dc897a1c44ec7f9ba2d2147bbe48578fef14
catherinekimani <catherinekimani882@gmail.com>
2023-09-21 03:49:30 +0300

    Modify City model

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 import os
 
 
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
