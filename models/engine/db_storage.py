a0cbad3b05429567058c205065dda328b5600b2a
catherinekimani <catherinekimani882@gmail.com>
2023-09-21 04:14:30 +0300

    Update file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,78 +11,63 @@ from models.user import User
 from models.city import City
 from models.state import State
 
+user = os.getenv('HBNB_MYSQL_USER')
+pwd = os.getenv('HBNB_MYSQL_PWD')
+host = os.getenv('HBNB_MYSQL_HOST')
+db = os.getenv('HBNB_MYSQL_DB')
+env = os.getenv('HBNB_ENV')
+
 
 class DBStorage:
     """class to manages storage of hbnb models in SQL db"""
+    __classes = [State, City, User, Place, Review, Amenity]
     __engine = None
     __session = None
 
     def __init__(self):
         """Initializes SQL database storage"""
-        user = os.getenv('HBNB_MYSQL_USER')
-        pword = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db_name = os.getenv('HBNB_MYSQL_DB')
-        env = os.getenv('HBNB_ENV')
-        DATABASE_URL = "mysql+mysqldb://{}:{}@{}:3306/{}".format(
-            user, pword, host, db_name
-        )
-        self.__engine = create_engine(
-            DATABASE_URL,
-            pool_pre_ping=True
-        )
-        if env == 'test':
-            Base.metadata.drop_all(self.__engine)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            user, pwd, host, db), pool_pre_ping=True)
+    if env == "test":
+        Base.MetaData.drop_all()
 
     def all(self, cls=None):
         """Returns a dict of models in storage"""
-        objects = dict()
-        all_cls = (User, State, City, Amenity, Place, Review)
-        if cls is None:
-            for class_type in all_cls:
-                query = self.__session.query(class_type)
-                for obj in query.all():
-                    obj_keys = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                    objects[obj_keys] = obj
-        else:
-            query = self.__session.query(cls)
-            for obj in query.all():
-                obj_keys = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                objects[obj_keys] = obj
-        return objects
+        m_dict = {}
+        if cls in self.__classes:
+            resl = DBStorage.__session.query(cls)
+            for row in resl:
+                key = "{}.{}".format(row.__class__.__name__, row.id)
+                m_dict[key] = row
+        elif cls is None:
+            for cl in self.__classes:
+                resl = DBStorage.__session.query(cl)
+                for row in resl:
+                    key = "{}.{}".format(row.__class__.__name__, row.id)
+                    m_dict[key] = row
+        return m_dict
 
     def new(self, obj):
         """Adds new object to storage"""
-        if obj is not None:
-            try:
-                self.__session.add(obj)
-                self.__session.flush()
-                self.__session.refresh(obj)
-            except Exception as exc:
-                self.__session.rollback()
-                raise exc
+        DBStorage.__session.add(obj)
 
     def save(self):
         """Commits the session changes"""
-        self.__session.commit()
+        DBStorage.__session.commit()
 
     def delete(self, obj=None):
         """Removes an object from the db"""
-        if obj is not None:
-            self.__session.query(type(obj)).filter(
-                type(obj).id == obj.id).delete(
-                synchronize_session=False
-            )
+        DBStorage.__session.delete(obj)
 
     def reload(self):
         """Loads storage db"""
+
         Base.metadata.create_all(self.__engine)
-        SessionFactory = sessionmaker(
-            bind=self.__engine,
-            expire_on_commit=False
-        )
-        self.__session = scoped_session(SessionFactory)()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        DBStorage.__session = Session()
 
     def close(self):
         """Close"""
-        self.__session.close()
+        DBStorage.__session.close()
