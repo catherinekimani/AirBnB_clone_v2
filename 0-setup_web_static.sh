6b1dcb2d857233762df95637a05d9863e4f0f978
catherinekimani <catherinekimani882@gmail.com>
2023-10-06 05:45:03 +0300

    script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# set up web servers for deployment of web static
+
+# Install Nginx
+sudo apt-get update
+sudo apt-get install -y nginx
+
+# create folders
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
+
+# Create a fake HTML file
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+	</html>" >> /data/web_static/releases/test/index.html
+
+# create symbolink & recreate it too
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership to ubuntu user, group
+sudo chown -R ubuntu:ubuntu /data/
+
+# update nginx config to server the content of /data/web_static/current/ to hbnb_static
+sudo sed -i "24i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+# restart nginx for chnages to be applied
+sudo service nginx restart
