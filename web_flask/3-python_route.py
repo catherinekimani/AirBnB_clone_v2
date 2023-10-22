c01082c891d7a25f4cc65b17a69ca53a2804f60d
catherinekimani <catherinekimani882@gmail.com>
2023-10-22 18:03:04 +0300

    Script that displays python followed by the text value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ routing to root """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ routing to /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Return given text """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Display 'Python' followed by  the value of the text """
+    return "Python {}".format(text.replace('_', " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
