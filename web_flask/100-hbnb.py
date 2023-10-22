73c938f2c88d6c002f632a744b8c8a8bbd3ae3f7
catherinekimani <catherinekimani882@gmail.com>
2023-10-23 01:02:49 +0300

    Implement flask web application with cities & places display

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Starts a flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Tear down db """
+    storage.close()
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_filters():
+    """ Display HBNB filters page """
+    return render_template(
+        '100-hbnb.html',
+        states=storage.all(State), amenities=storage.all(Amenity),
+        places=storage.all(Place))
