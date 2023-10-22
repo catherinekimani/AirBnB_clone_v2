0ef2236315bde8330e865a84999a55663626d3c8
catherinekimani <catherinekimani882@gmail.com>
2023-10-22 18:07:40 +0300

    Script that lists state ids

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Tear down db """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ List of state ids """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states())
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
